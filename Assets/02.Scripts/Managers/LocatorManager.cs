e225be3ff2e9ac5501947fcbfe799441c5289857
Real-pine <crewger9030@gmail.com>
2024-11-28 13:16:41 +0900

    [Complex]FrameWork 진행
    Add: 기본이 되는 폴더생성 및 폴더넘버링 진행
    Add: 각 매니저들이 상속받을 IManager.cs 생성
    Add: 싱글톤 클래스가 상속받을 MonoSingleton.cs생성
    Add: UI클래스가 상속받을 UIBase.cs생성
    Add: 기본이 될 것 같은 매니저클래스들 생성
    Add: 중개자 역할을 할 LocatorManager.cs 생성

---
+++ Assets/02.Scripts/Managers/LocatorManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public interface IManager
+{
+    // LocatorManager를 제외한 각 매니저들은 IManager를 상속받아야함
+    public void Initialize();
+}
+
+public class LocatorManager : MonoSingleton<LocatorManager>
+{
+    DataManager dataManager = new DataManager();
+    TurnManager turnManager = new TurnManager();
+    BattleManager battleManager = new BattleManager();
+    SoundManager soundManager = new SoundManager();
+    
+    public override void Awake()
+    {
+        base.Awake();
+        
+        // 여기서 각 매니저들 초기화 순서 관리가능
+        // dataManager.Initialize();
+        // soundManager.Initialize();
+        // turnManager.Initialize();
+        // battleManager.Initialize();
+    }
+    
+    // 각 매니저들은 MonoBehaviour를 상속받고있지 않으므로 MonoBehaviour에 관한 함수들은 
+    // 로케이터매니저에서 대신 만든다음 로케이터를 통해 실행해줘야함.
+    // 아래의 StartCoRoutines함수가 예시임. 
+    // 다른매니저에서 코루틴 사용은 LocaterManager.Instance.StartCoRoutines(사용할코루틴);
+    
+    // StartCoroutine과 구별하기위해 이름을 살짝 바꿈.
+    public void StartCoRoutines(IEnumerator coroutine)
+    {
+        StartCoroutine(coroutine);
+    }
+    
+}
