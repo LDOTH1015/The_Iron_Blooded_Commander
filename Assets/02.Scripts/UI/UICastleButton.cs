d0a6897faea00e48d8eff7e02e0107c8bada88f3
Real-pine <crewger9030@gmail.com>
2024-11-28 21:46:51 +0900

    [Chore] 버튼 캐싱 작업
    Chore UICastleButton.cs에서 버튼캐싱작업진행

--- Assets/02.Scripts/UI/UICastleButton.cs
+++ Assets/02.Scripts/UI/UICastleButton.cs
@@ -3,14 +3,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class UICastleButton : UIBase, IPointerEnterHandler, IPointerExitHandler
 {
     private GameObject uiCastleOutLine;
-
+    private Button button;
     private void Start()
     {
         uiCastleOutLine = transform.GetChild(0).gameObject;
+        button = transform.GetChild(1).gameObject.GetComponent<Button>();
     }
 
     public void OnPointerEnter(PointerEventData eventData)
