fc73447b4c79ee0772e7aaba26b8d73dd8b533d1
OTH <112844098+LDOTH1015@users.noreply.github.com>
2024-11-29 19:36:43 +0900

    [Add] ExcelToJsonData

--- Assets/02.Scripts/Managers/DataManager.cs
+++ Assets/02.Scripts/Managers/DataManager.cs
@@ -2,11 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DataManager : IManager
+public class DataManager : MonoSingleton<DataManager>, IManager
 {
+    public KnightInfo knightInfo;
+    public UnitTypeInfo unitTypeInfo;
+    public UnitDivisionInfo unitDivisionnfo;
+
     public void Initialize()
     {
-        throw new System.NotImplementedException();
+        var KnightJson= Resources.Load("JsonData/KnightDataTable") as TextAsset;
+        var UnitTypeJson = Resources.Load("JsonData/UnitTypeDataTable") as TextAsset;
+        var UnitDivisionJson = Resources.Load("JsonData/UnitDivisionDataTable") as TextAsset;
+        knightInfo = JsonUtility.FromJson<KnightInfo>(KnightJson.ToString());
+        unitTypeInfo = JsonUtility.FromJson<UnitTypeInfo>(UnitTypeJson.ToString());
+        unitDivisionnfo = JsonUtility.FromJson<UnitDivisionInfo>(UnitDivisionJson.ToString());
     }
-    
 }
