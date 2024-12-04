8a8f38cab4861a187af3bd4a84b69fcbe9226721
Real-pine <crewger9030@gmail.com>
2024-12-04 20:47:17 +0900

    [Feat] 이벤트결과업데이트 구현
    Feat 이벤트 결과를 업데이트 구현

--- Assets/02.Scripts/TurnState/WorldTurnState.cs
+++ Assets/02.Scripts/TurnState/WorldTurnState.cs
@@ -7,18 +7,19 @@ public class WorldTurnState : ITurnState
     // 메인화면 UI전환정도?
     public void Enter()
     {
-        throw new System.NotImplementedException();
+        // 현재 내용 없음
     }
     
     // 고정이벤트 타임라인에 넣기, 확률이벤트발생 시 타임라인에 넣기
     public void Execute()
     {
-        throw new System.NotImplementedException();
+        // 현재 내용 없음
+        Exit();
     }
     
     // TransitionToPlayerTurn
     public void Exit()
     {
-        throw new System.NotImplementedException();
+        LocatorManager.Instance.turnManager.TransitionTo(LocatorManager.Instance.turnManager.playerTurnState);
     }
 }
