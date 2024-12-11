f5766090f7851eccd8e43c0a46d5036eca686138
Mainoo-developer <hyunw0629@naver.com>
2024-12-11 14:41:40 +0900

    [Add] AIManager & Animation

---
+++ Assets/02.Scripts/Animation/AnimationController.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    private Animator animator;
+
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // 현재 속도는 대략적으로 정의 되어 있음
+    public void PlayIdle()
+    {
+        animator.SetFloat("Speed", 0);
+    }
+
+    public void PlayWalk()
+    {
+        animator.SetFloat("Speed", 0.5f);
+    }
+
+    public void PlayRun()
+    {
+        animator.SetFloat("Speed", 1f);
+    }
+
+    public void PlayCombatIdle()
+    {
+        animator.SetBool("IsInCombat", true);
+    }
+
+    public void ResetCombatIdle()
+    {
+        animator.SetBool("IsInCombat", false);
+    }
+
+    public void PlayAttack()
+    {
+        animator.SetTrigger("IsAttack");
+    }
+
+    public void PlayHit()
+    {
+        animator.SetTrigger("IsHit");
+    }
+
+    public void PlayDeath()
+    {
+        animator.SetBool("IsDead", true);
+    }
+}
